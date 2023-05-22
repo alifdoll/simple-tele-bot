1cc677bff681421496daed7b3862ccbfa5d0b97d
alifdoll <alifianfajarpratama@gmail.com>
2023-05-22 12:52:19 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-import { Telegraf } from "telegraf";
+const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.start((ctx) => ctx.reply("Halo"));
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch();
